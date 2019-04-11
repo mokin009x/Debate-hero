3b69cab002c2024e84ef90e90f73031dbcddb5dd
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-04-11 09:35:46 +0200

    crowd testing

--- Assets/Scripts/Crowd.cs
+++ Assets/Scripts/Crowd.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Crowd : MonoBehaviour
 {
     float c;
     GameObject crowd;
+    public Transform defaultPos;
+    public Vector3 blueWinning;
+    public Vector3 redWinning;
 
     // Start is called before the first frame update
     void Start()
     {
         crowd = transform.Find("crowd").gameObject;
+        defaultPos = crowd.transform;
+        redWinning = new Vector3(1, crowd.transform.position.y, crowd.transform.position.z);
+        blueWinning = new Vector3(-1, crowd.transform.position.y, crowd.transform.position.z);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        c = PlayerPrefs.GetInt("Crowd");
+        if ()
+        {
+            
+        }
 
-        crowd.transform.localPosition = new Vector3(0, (c - 25) / 25, 0);
     }
 }
